90ab1408d1f964012d405b972ba5af9fe1211fd4
DESKTOP-B4S3HSP\zinno <bohdan.zashko@magnise.com>
2019-10-25 21:00:20 +0300

    Added data transfer objects for customer and transactions.

---
+++ CustomersDataPovider/BusinessLogicLayer/DataTransferObjects/CustomerDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BusinessLogicLayer.DataTransferObjects
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class CustomerDTO
+    {
+        #region Properties
+
+        public Int32 CustomerID { get; set; }
+
+        public String Name { get; set; }
+
+        public String Email { get; set; }
+
+        public String Mobile { get; set; }
+
+        public ICollection<TransactionDTO> Transactions { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public CustomerDTO()
+        {
+            Transactions = new List<TransactionDTO>();
+        }
+        
+        #endregion
+
+    }
+}
---
+++ CustomersDataPovider/BusinessLogicLayer/DataTransferObjects/TransactionDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BusinessLogicLayer.DataTransferObjects
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class TransactionDTO
+    {
+        #region Properties
+
+        public Int32 Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Double Amount { get; set; }
+
+        public String Currency { get; set; }
+
+        public String Status { get; set; }
+
+        #endregion
+
+    }
+}
