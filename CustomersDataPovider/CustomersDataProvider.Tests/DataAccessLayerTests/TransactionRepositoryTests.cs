5af91b3ecf38eae000212125c43ff3919e3342f9
DESKTOP-B4S3HSP\zinno <bohdan.zashko@magnise.com>
2019-10-28 03:44:08 +0200

    Created unit tests for Transaction repository.

---
+++ CustomersDataPovider/CustomersDataProvider.Tests/DataAccessLayerTests/TransactionRepositoryTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using CustomersDataProvider.DataAccessLayer;
+using CustomersDataProvider.DataAccessLayer.Abstraction;
+using CustomersDataProvider.DataAccessLayer.Entities;
+using CustomersDataProvider.DataAccessLayer.Entities.Enums;
+using CustomersDataProvider.DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace CustomersDataProvider.Tests.DataAccessLayerTests
+{
+    [TestFixture]
+    class TransactionRepositoryTests
+    {
+        #region Fields
+
+        private IRepository<TransactionEntity> _transactionRepository;
+
+        #endregion
+
+        #region SetUp
+
+        [OneTimeSetUp]
+        public void SetUp()
+            => _transactionRepository = new GenericRepository<TransactionEntity>(new CustomersDBContext());
+
+        #endregion
+
+        #region GetByIdTests
+
+        [Test]
+        public async Task GetByIdTest1()
+        {
+            var transaction = await _transactionRepository.GetByIdAsync(1)
+                .ConfigureAwait(false);
+
+            AssertFirstTransaction(transaction);
+        }
+
+        [Test]
+        public async Task GetByIdTest2()
+        {
+            var transaction = await _transactionRepository.GetByIdAsync(45)
+                .ConfigureAwait(false);
+
+            Assert.AreEqual(null, transaction);
+        }
+
+        #endregion
+
+        #region GetTests
+
+        [Test]
+        public async Task GetTest1()
+        {
+            var transaction = await _transactionRepository.Get(x => x.CustomerId == 1)
+                .AsNoTracking()
+                .FirstOrDefaultAsync()
+                .ConfigureAwait(false);
+
+            AssertFirstTransaction(transaction);
+        }
+
+        [Test]
+        public async Task GetTest2()
+        {
+            var transaction = await _transactionRepository.Get(x => x.CustomerId == 45)
+                .AsNoTracking()
+                .SingleOrDefaultAsync()
+                .ConfigureAwait(false);
+
+            Assert.AreEqual(null, transaction);
+        }
+
+        #endregion
+
+        #region Private
+
+        private void AssertFirstTransaction(TransactionEntity transaction)
+        {
+            Assert.NotNull(transaction);
+
+            Assert.AreEqual(1, transaction.Id);
+            Assert.AreEqual(1, transaction.CustomerId);
+            Assert.AreEqual(new DateTime(2007, 5, 8, 12, 34, 0), transaction.DateTime);
+            Assert.AreEqual(151345.54m, transaction.Amount);
+            Assert.AreEqual(Currency.EUR, transaction.Currency);
+            Assert.AreEqual(TransactionStatus.Success, transaction.Status);
+        }
+
+        #endregion
+
+    }
+}
